b8c9c7425301d2c6a1d14148b76568d20fd462bb
Vincent Monnot <vincent.monnot@edu.esiee.fr>
2022-05-13 15:10:55 +0200

    need to debug test

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+from jwt_utils import build_token, decode_token
 
 app = Flask(__name__)
 
@@ -10,6 +11,18 @@ def hello_world():
 @app.route('/quiz-info', methods=['GET'])
 def GetQuizInfo():
 	return {"size": 0, "scores": []}, 200
+
+@app.route('/login', methods=['POST'])
+def Login():
+	payload = request.get_json()
+	try:
+		password = payload['password']
+	except:
+		return "Not a JSON body, or missing password field", 500
+	if password == "Vive l'ESIEE !":
+		return build_token(), 200
+	else:
+		return "Wrong password", 401
 
 if __name__ == "__main__":
     app.run(ssl_context='adhoc')
