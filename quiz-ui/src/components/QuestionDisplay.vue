fe3e4303324632eadab9638334f6bd7b7ac954a7
Vincent Monnot <vincent.monnot@edu.esiee.fr>
2022-06-07 11:02:48 +0200

    correct response in red

--- quiz-ui/src/components/QuestionDisplay.vue
+++ quiz-ui/src/components/QuestionDisplay.vue
@@ -4,16 +4,21 @@
     <div class="card-body">
       <h5 class="card-title">{{ question.title }}</h5>
       <p class="card-text">{{ question.text }}</p>
-      <div class="btn-group" style="flex-wrap: wrap;" role="group">
+      <div v-if="!this.admin" class="btn-group" style="flex-wrap: wrap;" role="group">
         <div v-for="(answerEntry, index) in this.question.possibleAnswers" v-bind:key="answerEntry.text">
-          <button v-if="!this.admin" class="btn btn-primary mr-2 mb-1" @click="$emit('answer-selected', index,answerEntry.isCorrect)">
+          <button class="btn btn-primary mr-2 mb-1" @click="$emit('answer-selected', index,answerEntry.isCorrect)">
             {{ answerEntry.text }}
           </button>
-          <p v-if="this.admin" @click="$emit('answer-selected', index,answerEntry.isCorrect)">
-            {{index}} - {{ answerEntry.text }} -{{ answerEntry.isCorrect }}
-          </p>
         </div>
       </div>
+
+      <ul  v-if="this.admin" class="list-group">
+        <div v-for="(answerEntry, index) in this.question.possibleAnswers" v-bind:key="answerEntry.text">
+          <li class="list-group-item list-group-item-action" :class="{ 'text-danger font-weight-bold': answerEntry.isCorrect }">
+            {{index}} - {{ answerEntry.text }} - {{ answerEntry.isCorrect }}
+          </li>
+        </div>
+      </ul>
     </div>
   </div>
   
